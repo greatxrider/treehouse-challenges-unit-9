c2d2a5e4381ee72211d7785d31f1fce32986d511
greatxrider <daligdig.jephmari@gmail.com>
2024-08-31 16:51:43 +0800

    restApi restructuring

--- codeAlong/app.js
+++ codeAlong/app.js
@@ -1,44 +1,24 @@
 const express = require('express');
 const app = express();
+const quotesRouter = require('./routes');
 
-const records = require('./records');
+app.use(express.json());
+app.use('/api', quotesRouter);
 
-// SEND a GET request to /quotes READ a list of quotes
-app.get('/quotes', async (req, res) => {
-    const quotes = await records.getQuotes();
-    res.json(quotes);
+// Error Handling
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
 });
 
-// Send a Get request to /quotes/:id READ(view) a quote
-app.get('/quotes/:id', async (req, res) => {
-    let id = req.params.id;
-    const quote = await records.getQuote(id);
-    res.json(quote);
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    });
 });
-
-// Send a POST request to /quotes to CREATE a new quote
-// Send a PUT request to /quotes/:id UPDATE (edit) a quote
-// Send a DELETE request to /quotes/:id DELETE a quote
-// Send a GET Request to /quotes/quote/random READ (view) a random quote
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
-
-const data = {
-    quotes: [
-        {
-            id: 8721,
-            quote: "We must accept finite disappointment, but we must never lose infinite hope.",
-            author: "Martin Luther King"
-        },
-        {
-            id: 5779,
-            quote: "Use what you’ve been through as fuel, believe in yourself and be unstoppable!",
-            author: "Yvonne Pierre"
-        },
-        {
-            id: 3406,
-            quote: "To succeed, you have to do something and be very bad at it for a while. You have to look bad before you can look really good.",
-            author: "Barbara DeAngelis"
-        }
-    ]
-}
---
+++ codeAlong/routes.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const quotesRouter = express.Router();
+
+const records = require('./records');
+
+function asyncHandler(cb) {
+    return async (req, res, next) => {
+        try {
+            await cb(req, res, next);
+        } catch (err) {
+            next(err);
+        }
+    };
+}
+
+// SEND a GET request to /quotes READ a list of quotes
+quotesRouter.get('/quotes', asyncHandler(async (req, res) => {
+    const quotes = await records.getQuotes();
+    res.json(quotes);
+}));
+
+// Send a Get request to /quotes/:id READ(view) a quote
+quotesRouter.get('/quotes/:id', asyncHandler(async (req, res) => {
+    let id = req.params.id;
+    const quote = await records.getQuote(id);
+    if (quote) {
+        res.json(quote);
+    } else {
+        res.status(404).json({ message: "Quote not found." });
+    }
+}));
+
+// Send a POST request to /quotes to CREATE a new quote
+quotesRouter.post('/quotes', asyncHandler(async (req, res) => {
+    if (req.body.author && req.body.quote) {
+        const quote = await records.createQuote({
+            quote: req.body.quote,
+            author: req.body.author
+        });
+        res.status(201).json(quote);
+    } else {
+        res.status(400).json({ message: "Quote and author required." });
+    }
+}));
+
+// Send a PUT request to /quotes/:id UPDATE (edit) a quote
+quotesRouter.put('/quotes/:id', asyncHandler(async (req, res) => {
+    const quote = await records.getQuote(req.params.id);
+    if (quote) {
+        quote.quote = req.body.quote;
+        quote.author = req.body.author;
+
+        await records.updateQuote(quote);
+        res.status(204).end();
+    } else {
+        res.status(400).json({ message: "Quote Not Found" });
+    }
+}));
+
+// Send a DELETE request to /quotes/:id DELETE a quote
+quotesRouter.delete("/quotes/:id", asyncHandler(async (req, res, next) => {
+    const quote = await records.getQuote(req.params.id);
+    records.deleteQuote(quote);
+    res.status(204).end();
+}));
+
+// Send a GET Request to /quotes/quote/random READ (view) a random quote
+
+module.exports = quotesRouter;
