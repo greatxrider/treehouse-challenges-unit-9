a247872324c30c41162e99b3886879ecbb96c6c6
greatxrider <daligdig.jephmari@gmail.com>
2024-09-03 15:02:54 +0800

    update status and routes

--- codeAlong/dataValidationLibrary/routes.js
+++ codeAlong/dataValidationLibrary/routes.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { check, validationResult } = require('express-validator');
 const express = require('express');
 const bcrypt = require('bcrypt');
 
@@ -15,42 +16,71 @@ router.get('/users', (req, res) => {
   res.json(users);
 });
 
-router.post('/users', (req, res) => {
-  // Get the user from the request body.
-  const user = req.body;
+// Storing ValidationChain on a variable
+// const nameValidationChain = check('name')
+//   .exists({ checkNull: true, checkFalsy: true })
+//   .withMessage('Please provide a value for "name"');
 
-  const errors = [];
+// const emailValidationChain = check('email')
+//   .exists({ checkNull: true, checkFalsy: true })
+//   .withMessage('Please provide a value for "email"');
 
-  // Validate that we have a `name` value.
-  if (!user.name) {
-    errors.push('Please provide a value for "name"');
-  }
+router.post('/users', [
+  check('name')
+    .exists()
+    .withMessage('Please provide a value for "name"'),
+  check('email')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "email"')
+    .isEmail()
+    .withMessage('Please provide a valid email address'),
+  check('birthday')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "birthday"')
+    .isISO8601()
+    .withMessage('Please provide a valid date for "birthday"'),
+  check('password')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "password"')
+    .isLength({ min: 8, max: 20 })
+    .withMessage('Password must be between 8 and 20 characters long'),
+  check('confirmedPassword')
+    .exists({ checkNull: true, checkFalsy: true })
+    .withMessage('Please provide a value for "confirmedPassword"')
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage('Confirmed password does not match the password')
+], async (req, res) => {
 
-  // Validate that we have an `email` value.
-  if (!user.email) {
-    errors.push('Please provide a value for "email"');
-  }
+  // Attempt to get the validation result from the Request object.
+  const errors = validationResult(req);
 
-  // Validate that we have a `password` value.
-  let password = user.password;
-  if (!password) {
-    errors.push('Please provide a value for "password"');
-  } else if (password.length < 8 || password.length > 20) {
-    errors.push('Your password should be between 8 and 20 characters');
-  } else {
-    user.password = bcrypt.hashSync(password, 10);
-  }
+  // If there are validation errors...
+  if (!errors.isEmpty()) {
+    // Use the Array `map()` method to get a list of error messages.
+    const errorMessages = errors.array().map(error => error.msg);
 
-  // If there are any errors...
-  if (errors.length > 0) {
     // Return the validation errors to the client.
-    res.status(400).json({ errors });
+    res.status(400).json({ errors: errorMessages });
   } else {
-    // Add the user to the `users` array.
-    users.push(user);
+    // Get the user from the request body.
+    const { name, email, birthday, password } = req.body;
 
-    // Set the status to 201 Created and end the response.
-    res.status(201).end();
+    try {
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Create a new user object with the hashed password
+      const user = { name, email, birthday, password: hashedPassword };
+
+      // Add the user to the `users` array.
+      users.push(user);
+
+      // Set the status to 201 Created and end the response.
+      res.status(201).end();
+    } catch (error) {
+      // Handle any errors that occur during hashing
+      res.status(500).json({ error: 'Internal server error' });
+    }
   }
 });
 
